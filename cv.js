4bdbcdb6e87a3b165c66cadb3b936c89bf1a67a4
KaHongTam <kahongtam@hotmail.com>
2018-11-14 14:13:13 +0100

    werkende plaatjes

--- cv.js
+++ cv.js
@@ -1,11 +1,33 @@
 $(document).ready(function(){
     $(".tp").hide();
+    $(".tp1").hide();
+    $(".cz").hide();
+    
     $(".experience_tp").on({
         mouseenter: function(){
             $(".tp").fadeIn(350);
         },  
         mouseleave: function(){
-            $(".tp").fadeOut(350);
+            $(".tp").fadeOut(650);
         }, 
     });
+
+    $(".experience_tp1").on({
+        mouseenter: function(){
+            $(".tp1").fadeIn(350);
+        },  
+        mouseleave: function(){
+            $(".tp1").fadeOut(650);
+        }, 
+    });
+
+    $(".experience_cz").on({
+        mouseenter: function(){
+            $(".cz").fadeIn(350);
+        },  
+        mouseleave: function(){
+            $(".cz").fadeOut(650);
+        }, 
+    });
+
 });
