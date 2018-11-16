f97d8ae586f82bcab5ede8fe03e308f7ce75c25f
KaHongTam <kahongtam@hotmail.com>
2018-11-16 11:52:02 +0100

    scrollen vertraagd ipv 50px bij 150 px

--- projects.js
+++ projects.js
@@ -1,8 +1,8 @@
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 150px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 150) {
         document.getElementById("myBtn").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
